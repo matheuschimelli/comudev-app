e9d041e0868f2eb282c0e6fd8ec51cb578a9d145
Matheus Chimelli <matheuschimelli7@gmail.com>
2022-11-21 21:34:49 -0300

    updates editor

--- src/Codemirror.js
+++ src/Codemirror.js
@@ -3,45 +3,45 @@ import CodeMirror from '@uiw/react-codemirror';
 import { dracula } from '@uiw/codemirror-theme-dracula';
 import { javascript } from '@codemirror/lang-javascript';
 import { useState, useEffect } from 'react';
-import { Grid } from '@mui/material';
+import { Box } from "@chakra-ui/react"
 import io from 'socket.io-client'
 
-function Editor() {
-  
+function Editor({ roomId }) {
   const [userCode, setUserCode] = useState("");
+
   useEffect(() => {
+    alert(roomId)
+  }, [roomId])
 
-  },[userCode])
-
-  const handleCodeMirrorOnChange = (value, viewUpdate) =>{
+  const handleCodeMirrorOnChange = (value, viewUpdate) => {
     setUserCode(value)
   }
 
-    return (
-        <>
-          <Grid container>
-            <Grid item xs={7}>
-              <CodeMirror
-                value="console.log('hello world!');"
-                height="700px"
-                width = "auto"
-                theme={dracula}
-                extensions={[javascript({ jsx: true })]}
-                onChange={handleCodeMirrorOnChange}
-              />
-            </Grid>
-            <Grid item xs={5}>
-              <iframe
-                className="preview"
-                srcDoc={userCode}
-                title="output"
-                sandbox="allow-scripts"
-                frameBorder="0"
-              />
-            </Grid>
-          </Grid> 
-        </>
-        
-      );
+  return (
+    <Box display="flex" flexDir="row">
+      <Box w="30%">
+        <CodeMirror
+          value="console.log('hello world!');"
+          height="700px"
+          width="auto"
+          theme={dracula}
+          extensions={[javascript({ jsx: true })]}
+          onChange={handleCodeMirrorOnChange}
+        />
+      </Box>
+      <Box>
+
+        <iframe
+          className="preview"
+          srcDoc={userCode}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+        />
+      </Box>
+
+    </Box>
+
+  );
 }
 export default Editor;
